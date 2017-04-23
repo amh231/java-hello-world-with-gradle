461e6f5205d18a5c57a41ec17d0b726e2e61fe6d
amh231 <amh231@txstate.edu>
2017-04-23 15:04:30 -0500

    Update TestGreeter.java

--- src/main/java/hello/TestGreeter.java
+++ src/main/java/hello/TestGreeter.java
@@ -7,6 +7,10 @@ public class TestGreeter {
     Greeter test1 = new Greeter("Allyn");
     Greeter test2 = new Greeter("Software Engineering");
     Greeter test3 = new Greeter("NAMESSSS!"); 
+    
+    System.out.println( test1.sayHello() );
+    System.out.println( test2.sayHello() );
+    System.out.println( test3.sayHello() );
   }
   
 }
